a93ac742be01a5328517d6c85c6fb16d1ca10662
Lawrenz <87652774+SenpaiLawrenz@users.noreply.github.com>
2022-09-29 18:50:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var url = document.getElementById("url");
+var go = document.getElementById("go");
+var browser = document.getElementById("browser");
+
+go.addEventListener("click", LoadPage);
+
+function LoadPage() {
+    var newUrl = url.value
+    browser.src = newUrl
+}
